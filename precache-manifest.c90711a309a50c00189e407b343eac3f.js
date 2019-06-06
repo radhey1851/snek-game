eb9251273d18683aaad82d722753ecb1e855d12c
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-06 05:58:36 +0530

    Updates

---
+++ precache-manifest.c90711a309a50c00189e407b343eac3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dab6a44fd42d657ec9c8ee4b004d064",
+    "url": "/snek-game/index.html"
+  },
+  {
+    "revision": "63c731627bdaf3028aef",
+    "url": "/snek-game/static/css/main.12c9a946.chunk.css"
+  },
+  {
+    "revision": "285bff2fec83f3504833",
+    "url": "/snek-game/static/js/2.d60fbdff.chunk.js"
+  },
+  {
+    "revision": "63c731627bdaf3028aef",
+    "url": "/snek-game/static/js/main.54fc7bcd.chunk.js"
+  },
+  {
+    "revision": "ee69ca5f4123a2fffd26",
+    "url": "/snek-game/static/js/runtime~main.98a74df3.js"
+  },
+  {
+    "revision": "964111990ca3937960147b80e8d07e61",
+    "url": "/snek-game/static/media/arcade.96411199.TTF"
+  },
+  {
+    "revision": "640249ca410d93cfaa136bf339590f83",
+    "url": "/snek-game/static/media/snake2.640249ca.png"
+  }
+]);
